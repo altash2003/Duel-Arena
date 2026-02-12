e578ce4fe9480d2e7756654f996f5a5a0e359436
altash2003 <altash2003@gmail.com>
2026-02-12 10:55:59 +0800

    Delete public directory

--- public/assets/realtime.js
+++
@@ -1,5 +0,0 @@
-import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
-export function connectRealtime() {
-  const token = localStorage.getItem("token");
-  return io("/", { auth: { token } });
-}
